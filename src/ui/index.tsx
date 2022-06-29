b114a58b5d512e82ac6456cd1957e386fd0be3cf
Yuu6883 <okumura6883@gmail.com>
2022-06-29 02:51:29 -0700

    sync load and ui changes

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -15,6 +15,8 @@ const ControlPanel = observer(
         const [prog, setProg] = useState<Program>(null);
 
         useEffect(() => {
+            if (!model) return () => {};
+
             const prog = new Program(model);
             prog.load().then((loaded) =>
                 loaded ? console.log(`Model loaded: ${model}`) : close()
@@ -26,137 +28,169 @@ const ControlPanel = observer(
         }, [model]);
 
         return (
-            prog && (
-                <>
-                    <div className="controls">
-                        <h3>{model}</h3>
-                        <p>
-                            size:{" "}
-                            {prog.MZ > 1
-                                ? `${prog.MX}x${prog.MY}x${prog.MZ}`
-                                : `${prog.MX}x${prog.MY}`}
-                            {"  "}
-                            seed: {prog.seed}
-                        </p>
-                        {prog.loading ? (
-                            <p>loading...</p>
-                        ) : (
-                            <div className="control-buttons">
-                                {prog.running ? (
-                                    <button
-                                        onClick={() => {
-                                            prog.paused
-                                                ? prog.resume()
-                                                : prog.pause();
-                                        }}
-                                    >
-                                        {prog.paused ? "resume" : "pause"}
-                                    </button>
-                                ) : (
-                                    <>
-                                        <button onClick={() => prog.start()}>
-                                            Start
-                                        </button>
+            <>
+                <div className="controls">
+                    <h3 id="model-select">{model}</h3>
+                    {prog && (
+                        <>
+                            {" "}
+                            <p>
+                                size:{" "}
+                                {prog.MZ > 1
+                                    ? `${prog.MX}x${prog.MY}x${prog.MZ}`
+                                    : `${prog.MX}x${prog.MY}`}
+                                {"  "}
+                                seed: {prog.seed}
+                            </p>
+                            {prog.loading ? (
+                                <p>loading...</p>
+                            ) : (
+                                <div className="control-buttons">
+                                    {prog.running ? (
                                         <button
-                                            onClick={() => prog.randomize()}
+                                            onClick={() => {
+                                                prog.paused
+                                                    ? prog.resume()
+                                                    : prog.pause();
+                                            }}
                                         >
-                                            Randomize Seed
+                                            {prog.paused ? "resume" : "pause"}
                                         </button>
-                                    </>
-                                )}
-                                {prog.paused && (
-                                    <button onClick={() => prog.step()}>
-                                        Step
-                                    </button>
-                                )}
-                                {prog.output && (
-                                    <button
-                                        onClick={() =>
-                                            saveAs(
-                                                new Blob([prog.output.buffer]),
-                                                prog.output.name
-                                            )
-                                        }
-                                    >
-                                        Download Output
-                                    </button>
-                                )}
-                                {rtx &&
-                                    prog.renderType !== "bitmap" &&
-                                    prog.MZ > 1 && (
+                                    ) : (
+                                        <>
+                                            <button
+                                                onClick={() => prog.start()}
+                                            >
+                                                Start
+                                            </button>
+                                            <button
+                                                onClick={() => prog.randomize()}
+                                            >
+                                                Randomize Seed
+                                            </button>
+                                        </>
+                                    )}
+                                    {prog.paused && (
+                                        <button onClick={() => prog.step()}>
+                                            Step
+                                        </button>
+                                    )}
+                                    {prog.output && (
                                         <button
+                                            id="vox-download"
                                             onClick={() =>
-                                                prog.toggleRender(
-                                                    prog.renderType === "voxel"
-                                                        ? "isometric"
-                                                        : "voxel"
+                                                saveAs(
+                                                    new Blob([
+                                                        prog.output.buffer,
+                                                    ]),
+                                                    prog.output.name
                                                 )
                                             }
                                         >
-                                            {prog.renderType === "voxel"
-                                                ? "Isometric"
-                                                : "Voxel"}
+                                            Download Output
                                         </button>
                                     )}
-                                {debug && (
-                                    <button onClick={() => prog.debug()}>
-                                        debug 😭
-                                    </button>
-                                )}
-                            </div>
-                        )}
-                    </div>
-                    <div className="speed">
-                        <label>Speed</label>
+                                    {rtx &&
+                                        prog.renderType !== "bitmap" &&
+                                        prog.MZ > 1 && (
+                                            <button
+                                                onClick={() =>
+                                                    prog.toggleRender(
+                                                        prog.renderType ===
+                                                            "voxel"
+                                                            ? "isometric"
+                                                            : "voxel"
+                                                    )
+                                                }
+                                            >
+                                                {prog.renderType === "voxel"
+                                                    ? "Isometric"
+                                                    : "Voxel"}
+                                            </button>
+                                        )}
+                                    {debug && (
+                                        <button
+                                            id="debug"
+                                            onClick={() => prog.debug()}
+                                        >
+                                            debug 😭
+                                        </button>
+                                    )}
+                                </div>
+                            )}
+                        </>
+                    )}
+                </div>
+                {prog && (
+                    <div id="speed">
+                        <label
+                            style={{
+                                left: `calc(${
+                                    ((prog.speed + 200) / 400) * 100
+                                }% + ${Math.max(50 - prog.speed, 0) / 10}px)`,
+                            }}
+                        >
+                            {prog.speed >= 0
+                                ? `x${prog.speed || 1}`
+                                : `${-prog.speed}ms`}
+                        </label>
                         <input
-                            className="speed-bar"
+                            id="speed-bar"
                             type="range"
-                            min="-200"
-                            max="200"
+                            min={-200}
+                            max={200}
+                            step={1}
                             value={prog.speed}
                             onChange={(e) =>
                                 (prog.speed = e.target.valueAsNumber)
                             }
                         />
                     </div>
-                </>
-            )
+                )}
+            </>
         );
     }
 );
 
-const App = () => {
-    const [loaded, setLoaded] = useState(false);
-    const [model, setModel] = useState<string>(null);
+Program.loadPalette();
+Program.listModels();
 
-    useEffect(() => {
-        Promise.all([Program.loadPalette(), Program.listModels()]).then(() =>
-            setLoaded(true)
-        );
-    }, []);
+const App = () => {
+    const [model, setModel] = useState<string>(null);
 
     return (
         <>
-            <div>
-                <h1>MarkovJunior</h1>
+            <div className="left-column">
+                <h1 id="title">MarkovJunior</h1>
                 <div className="model-list">
-                    {loaded &&
-                        [...Program.models.keys()].map((k) => (
-                            <div
-                                key={k}
-                                data-selected={model === k}
-                                onClick={() => setModel(k)}
-                            >
-                                {k}
-                            </div>
-                        ))}
+                    {[...Program.models.keys()].map((k) => (
+                        <div
+                            key={k}
+                            data-selected={model === k}
+                            onClick={() => setModel(k)}
+                        >
+                            {k}
+                        </div>
+                    ))}
                 </div>
             </div>
-
             <div id="canvas-container">
-                {model && (
-                    <ControlPanel model={model} close={() => setModel(null)} />
-                )}
+                <ControlPanel model={model} close={() => setModel(null)} />
+            </div>
+            <div id="mobile-select">
+                <select
+                    defaultValue="disabled"
+                    onChange={(e) => setModel(e.target.value)}
+                >
+                    <option value="disabled" disabled>
+                        SELECT MODEL
+                    </option>
+                    {[...Program.models.keys()].map((k) => (
+                        <option key={k} value={k}>
+                            {k}
+                        </option>
+                    ))}
+                </select>
             </div>
         </>
     );
