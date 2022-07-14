f55e8da94826e95132942ca68faf17ec64f3b6ee
Yuu6883 <okumura6883@gmail.com>
2022-07-14 04:25:47 -0700

    fix when model hasn't run

--- src/program.ts
+++ src/program.ts
@@ -507,6 +507,6 @@ export class Model {
         this.renderer.dispose();
         // point of this is to call RuleNode.searching.throw
         // which breaks the scope that keeps the webassembly instance "alive" (not gc'd)
-        this.ip.root.reset();
+        this.ip?.root.reset();
     }
 }
